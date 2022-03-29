63c4ccbc7995c5d6247268b0579163bd7db70550
coszach@hotmail.it <coszach@hotmail.it>
2022-03-29 15:27:31 +0200

    Snack5

---
+++ snack5.php
@@ -0,0 +1,6 @@
+<?php 
+  $paragrafo = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, ipsa. Eveniet, repudiandae pariatur quidem possimus distinctio quod eum reiciendis voluptates. Tempore totam beatae id, vel quae porro accusantium inventore voluptatum illum fugiat dolore iure? Molestiae veritatis dolorum voluptatibus repellendus ducimus.';
+  $paragrafo_separato = explode('. ', $paragrafo);
+  var_dump($paragrafo_separato);
+?>
+
