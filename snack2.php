309669df49ab5fbdf0593ca49f616d6beedfdc3e
coszach@hotmail.it <coszach@hotmail.it>
2022-03-30 10:13:15 +0200

    aggiunto controllo isset

--- snack2.php
+++ snack2.php
@@ -1,10 +1,12 @@
 <?php 
-  $name = $_GET['name'];
-  $mail = $_GET['mail'];
-  $age = $_GET['age'];
+  $name = isset($_GET['name']) ? $_GET['name'] : '';
+  $mail = isset($_GET['mail']) ? $_GET['mail'] : '';
+  $age = isset($_GET['age']) ? $_GET['age'] : '';
+
   if (strlen($name) > 3 && is_numeric($age) && strpos($mail, '@') && strpos($mail, '.', -4)){
     echo 'Accesso riuscito';
   } else {
-    echo 'Accesso negato';
-  }
+    echo 'Accesso negato, potrebbero mancare dei dati.';
+  };
+  
 ?>
